2aad3df651c536f5c1b1b4b81cabb70c4cf9591e
okankop <38323395+okankop@users.noreply.github.com>
2018-06-14 00:26:40 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(name='vidaug',
+                 version='0.1',
+                 description='Video Augmentation Library',
+                 url='https://github.com/okankop/vidaug',
+                 author='Okan Kopuklu',
+                 author_email='okankopuklu@gmail.com',
+                 license='MIT',
+                 packages=setuptools.find_packages(),
+                 zip_safe=False)
